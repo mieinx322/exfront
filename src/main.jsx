0ce74be63304bcd4717cc3b4767c715e4d0e7746
이  은지 <mieinx322@gmail.com>
2025-10-16 11:20:17 +0900

    나이수정

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   const name = '홍길동';
+  const age = 20;
   root.render(<StrictMode>
       <BrowserRouter basename={'/yum_r/'}>
         <App />
